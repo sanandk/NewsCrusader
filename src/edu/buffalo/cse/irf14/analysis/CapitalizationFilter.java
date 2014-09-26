e57df849dc80fb982a7f0f1c4e94a1c7068e33fb
Karthik <karthikj@buffalo.edu>
2014-09-25 20:40:18 -0400

    captilization update

--- src/edu/buffalo/cse/irf14/analysis/CapitalizationFilter.java
+++ src/edu/buffalo/cse/irf14/analysis/CapitalizationFilter.java
@@ -25,27 +25,53 @@ public class CapitalizationFilter extends TokenFilter {
 		current_token = t_stream.getCurrent();
 		if (null == current_token)
 			return false;
-		
-        if (current_token.getTermText().matches(camelCaseRegex)) {
-
-			if (previous_token == null || previous_token.lineEnd) {
-				// loop to check if not begining of line and captilization
-				// conditions
-                current_token.setTermText(current_token.getTermText().toLowerCase());
-				t_stream.replace(current_token);
-			} else if (t_stream.hasNext()) {
+		int k=0;
+		if(current_token.getTermText().matches(camelCaseRegex) && !current_token.lineEnd && t_stream.hasNext()){
+			
+			do{
 				current_token = t_stream.next();
-                if (current_token.getTermText().matches(camelCaseRegex)) {
+				if (current_token.getTermText().matches(camelCaseRegex) ) {
+					k++;
 					t_stream.remove();
 					previous_token = t_stream.previous();
-                    current_token.setTermText(previous_token.getTermText()+" "+current_token.getTermText());
-
+	                current_token.setTermText(previous_token.getTermText()+" "+current_token.getTermText());
 					t_stream.replace(current_token);
-				} else {
-					t_stream.previous();
+				}else{
+					current_token=t_stream.previous();
+					break;
 				}
-			}
+			}while(!current_token.lineEnd);
+			
 		}
+		if(k==0 && (previous_token == null || previous_token.lineEnd) && current_token.getTermText().matches(camelCaseRegex)){
+			// loop to check if not begining of line and captilization conditions
+            current_token.setTermText(current_token.getTermText().toLowerCase());
+			t_stream.replace(current_token);
+		}
+		
+		
+		
+		
+//        if (current_token.getTermText().matches(camelCaseRegex)) {
+//
+//			if (previous_token == null || previous_token.lineEnd) {
+//				// loop to check if not begining of line and captilization
+//				// conditions
+//                current_token.setTermText(current_token.getTermText().toLowerCase());
+//				t_stream.replace(current_token);
+//			} else if (t_stream.hasNext()) {
+//				current_token = t_stream.next();
+//                if (current_token.getTermText().matches(camelCaseRegex)) {
+//					t_stream.remove();
+//					previous_token = t_stream.previous();
+//                    current_token.setTermText(previous_token.getTermText()+" "+current_token.getTermText());
+//
+//					t_stream.replace(current_token);
+//				} else {
+//					t_stream.previous();
+//				}
+//			}
+//		}
 
 		if (t_stream.hasNext()) {
 			return true;
